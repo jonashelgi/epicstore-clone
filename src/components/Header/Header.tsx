0f9c5d90093911682103a83b9a371d4992d69520
IceJonas <jonas.helgi@hotmail.com>
2021-11-02 14:04:59 +0000

    added firebase

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Dropdown } from "react-bootstrap";
 import StickyBox from "react-sticky-box";
 
 import { IconLink, Icon } from "..";
@@ -6,8 +7,36 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <StickyBox>
-      <div className={styles.root}>
+    <>
+      <StickyBox>
+        <div className={styles.root}>
+          <div className={styles.firstBox}>
+            <div className={styles.mainLink}>
+              <IconLink to="discover" title="Discover" color="white" />
+            </div>
+            <div className={styles.link}>
+              <IconLink to="browse" title="Browse" />
+            </div>
+          </div>
+          <div className={styles.secondBox}>
+            <div className={styles.link}>
+              <IconLink to="wishlist" title="Wishlist" />
+            </div>
+            <div className={styles.number}>2</div>
+            <label>
+              <input
+                type="search"
+                placeholder="Search"
+                className={styles.search}
+              />
+            </label>
+            <div className={styles.icon}>
+              <Icon type="user" size="lg" />
+            </div>
+          </div>
+        </div>
+      </StickyBox>
+      <div className={styles.dropdown}>
         <div className={styles.firstBox}>
           <div className={styles.mainLink}>
             <IconLink to="discover" title="Discover" color="white" />
@@ -15,25 +44,12 @@ const Header = () => {
           <div className={styles.link}>
             <IconLink to="browse" title="Browse" />
           </div>
-        </div>
-        <div className={styles.secondBox}>
-          <div className={styles.link}>
+          <div className={styles.mainLink}>
             <IconLink to="wishlist" title="Wishlist" />
-          </div>
-          <div className={styles.number}>2</div>
-          <label>
-            <input
-              type="search"
-              placeholder="Search"
-              className={styles.search}
-            />
-          </label>
-          <div className={styles.icon}>
-            <Icon type="user" size="lg" />
           </div>
         </div>
       </div>
-    </StickyBox>
+    </>
   );
 };
 
